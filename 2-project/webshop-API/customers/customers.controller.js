1272a1cc508e66acaec6b99d8589e1cb1789663c
Nanna Berg <bergnm@hotmail.com>
2023-04-06 13:10:22 +0200

    set up routes and model functions for customers

---
+++ 2-project/webshop-API/customers/customers.controller.js
@@ -0,0 +1,17 @@
+import * as customerModel from "./customers.model.js"
+
+export async function getBasketContent(req, res) {
+
+}
+
+export async function createBasketForCustomer(req, res) {
+    
+}
+
+export async function addProductToBasket(req, res) {
+    
+}
+
+export async function removeProductFromBasket(req, res) {
+    
+}
--- 2-project/webshop-API/customers/customers.route.js
+++ 2-project/webshop-API/customers/customers.route.js
@@ -1,3 +1,20 @@
 import express from 'express'
+import { 
+    getBasketContent, 
+    createBasketForCustomer, 
+    addProductToBasket, 
+    removeProductFromBasket 
+} from './customers.controller.js'
 
-export const customersRouter = express.Router();
+
+export const customersRouter = express.Router()
+
+
+customersRouter.get("/customers/:customerId/baskets", getBasketContent)
+
+customersRouter.post("/customers/:customerId/baskets", createBasketForCustomer)
+
+customersRouter.put("/customers/:customerId/baskets/products/:productId", addProductToBasket)
+
+customersRouter.delete("/customers/:customerId/baskets/products/:productId", removeProductFromBasket)
+
