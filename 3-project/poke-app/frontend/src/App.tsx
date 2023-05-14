2f6799ec50d3a3bf96454f47d5717426562371b8
millacat <camillakd@msn.com>
2023-05-14 19:25:57 +0200

    Input data from user and save it in context

--- 3-project/poke-app/frontend/src/App.tsx
+++ 3-project/poke-app/frontend/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, createContext } from "react";
+import './App.css'
+import { LoginForm } from './forms'
+
+// User context with default values for setting User data
+export const SetUserContext = createContext({
+  setLoggedInUser: (user: string) => {},
+  setLoggedInUserId: (id: number) => {},
+})
+
+// Context for getting data about user
+export const UserContext = createContext({ user: "", id: -1 })
 
 function App() {
+  const [user, setLoggedInUser] = useState("")
+  const [id, setLoggedInUserId] = useState(-1)
+  const newSetUserContext = { setLoggedInUser, setLoggedInUserId }
+  const newGetUserContext = { user, id }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SetUserContext.Provider 
+          value={newSetUserContext} // Overwrite default context values with newSetUserContext
+      >
+          <LoginForm/>
+      </SetUserContext.Provider>
+      <p>{user} with id {id}</p>
     </div>
   );
 }
--- 3-project/poke-app/frontend/src/forms.tsx
+++ 3-project/poke-app/frontend/src/forms.tsx
@@ -1,32 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Container, Form, Row, Col } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from "react-bootstrap/Button"
+import { SetUserContext } from "./App";
 
 export type SignUpData = {
-    Username : string
-    Password : string
+    FirstName : string
+    LastName : string
+    Email : string
 }
 
 type SignUpError = { 
-    Username? : string
-    Password? : string
+    FirstName? : string
+    LastName? : string
+    Email? : string
 }
-const PASSWORD = "Password"
-const USERNAME = "Username"
+const EMAIL = "Email"
+const FIRSTNAME = "FirstName"
+const LASTNAME = "LastName"
 
-export function SignUpForm() {
-    const [SignUpData, setSignUpData] = useState<SignUpData>({ Username : "", Password : "" })
+export function LoginForm() {
+    const [SignUpData, setSignUpData] = useState<SignUpData>({ FirstName : "", LastName : "", Email : "" })
     const [errors, setError] = useState<SignUpError>({})
+    const { setLoggedInUser, setLoggedInUserId } = useContext(SetUserContext)
 
-    function errorExists() { return errors.hasOwnProperty(USERNAME) || errors.hasOwnProperty(PASSWORD) }
-
+    function errorExists() { return errors.hasOwnProperty(FIRSTNAME) || errors.hasOwnProperty(LASTNAME) || errors.hasOwnProperty(EMAIL) }
+    
     function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
         const inputName = event.target.name
         const inputValue = event.target.value
         // Use indexed property of SignUpData state variable to set it
-        // Merges previous state object with new object { inputName : inputValue },
-        // that only sets one of the properties
+        // Merges previous state object with new object { inputName : inputValue }, that only sets one of the properties
         setSignUpData(previousSignUpDataState => (
             { ...previousSignUpDataState, ...{ [inputName] : inputValue } }
         ))
@@ -35,30 +39,49 @@ export function SignUpForm() {
     function validateInput() {
         let validationError : SignUpError = {}
 
-        if (!SignUpData.Username) { 
-            validationError.Username = "User name is required"
-        } else if (SignUpData.Username.length > 16) {
-            validationError.Username = "User name must be 16 characters or less"
+        if (!SignUpData.FirstName) { 
+            validationError.FirstName = "First name is required"
+        } else if (SignUpData.FirstName.length > 16) {
+            validationError.FirstName = "First name must be 16 characters or less"
         }
 
-        if (!SignUpData.Password) {
-            validationError.Password = "Password is required"
-        } else if (SignUpData.Password.length < 8) {
-            validationError.Password = "Password must use at least 8 characters"
+        if (!SignUpData.LastName) { 
+            validationError.LastName = "Last name is required"
+        } else if (SignUpData.LastName.length > 16) {
+            validationError.LastName = "Last name must be 16 characters or less"
         }
-
+        if (!SignUpData.Email) {
+            validationError.Email = "Email is required"
+        } else if (! /@/.test(SignUpData.Email)) { // Use regular expression
+            validationError.Email = "Email must contain '@'"
+        }
         setError(validationError)
 
-        return errorExists()
+        return !errorExists()
     }
 
     function onSubmit(event: React.FormEvent) {
         event.preventDefault()
-        console.log({currentState:SignUpData, errors: errors})
         const isValid = validateInput()
         if (isValid) {
-            // Call backend to add data
-            // useEffect() 
+                // Call backend to add user data                 
+                const user = {
+                  "firstName": SignUpData.FirstName,
+                  "lastName": SignUpData.LastName,
+                  "mail": SignUpData.Email 
+                }
+               // event.preventDefault()
+                fetch('http://localhost:3005/customers', {
+                    method: 'POST',
+                    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+                    body: JSON.stringify(user),
+                })
+                .then(userResult => userResult.json())
+                .then(js => { console.log({js:js})
+                    setLoggedInUser(js.firstName + js.lastName)
+                    setLoggedInUserId(js.id)
+                }) // save logged in user somewhere, so we can use it.
+                .catch(error => console.log({ error: error }))
         }
     }
 
@@ -68,48 +91,60 @@ export function SignUpForm() {
                 <Col sm={6}>
                 <h1>Sign up</h1>
                 <Form onSubmit={onSubmit}>
-                    <Form.Group >
-                        <Row>
-                            <Col sm={4}>
-                                <Form.Label htmlFor='userName'> User name </Form.Label>
-                            </Col>
-                            <Col sm={8}>
-                                <Form.Control
-                                    id='userName-input'
-                                    name='Username'
-                                    placeholder='Enter user name'
-                                    onChange={handleInputChange}
-                                />
-                            </Col>
-                        </Row>
+                    <Form.Group className="row">
+                        <Col sm={4}>
+                            <Form.Label htmlFor='FirstName'> First name </Form.Label>
+                        </Col>
+                        <Col sm={8}>
+                            <Form.Control
+                                id='FirstName-input'
+                                name='FirstName'
+                                placeholder='Enter first name'
+                                onChange={handleInputChange}
+                            />
+                        </Col>
                     </Form.Group>
                     <br/><br/>
-                    <Form.Group className="mb-3" controlId="passwordForm">
-                        <Row>
-                            <Col sm={4}>
-                                <Form.Label htmlFor='password'> Password </Form.Label>
-                            </Col>
-                            <Col sm={8}>
-                                <Form.Control 
-                                    id='password-input'
-                                    name='Password'
-                                    placeholder='Enter password'
-                                    onChange={handleInputChange}
-                                />
-                            </Col>
-                        </Row>
+                    <Form.Group className="row">
+                        <Col sm={4}>
+                            <Form.Label htmlFor='LastName'> Last name </Form.Label>
+                        </Col>
+                        <Col sm={8}>
+                            <Form.Control
+                                id='LastName-input'
+                                name='LastName'
+                                placeholder='Enter last name'
+                                onChange={handleInputChange}
+                            />
+                        </Col>
+                    </Form.Group>
+                    <br/><br/>
+                    <Form.Group className="row" controlId="EmailForm">
+                        <Col sm={4}>
+                            <Form.Label htmlFor='Email'> Email </Form.Label>
+                        </Col>
+                        <Col sm={8}>
+                            <Form.Control 
+                                id='Email-input'
+                                name='Email'
+                                placeholder='Enter Email'
+                                onChange={handleInputChange}
+                            />
+                        </Col>
                     </Form.Group>
                     <Row>
-                        <Col sm={3}/>
-                        <Col sm={9}>
-                            { errors.Username ? <span style={{color:'red', textAlign:'left'}}> { errors.Username } | </span> : null }
-                            { errors.Password ? <span style={{color:'red'}}> { errors.Password }</span> : null }
+                        <Col sm={4}/>
+                        <Col sm={8}>
+                            { errors.FirstName ? <span style={{color:'red', textAlign:'left'}}> { errors.FirstName } | </span> : null }
+                            { errors.LastName ? <span style={{color:'red'}}> { errors.LastName } | </span> : null }
+                            { errors.Email ? <span style={{color:'red'}}> { errors.Email }</span> : null }
                         </Col>
                     </Row>
                     <div>
                         <Button
                             type='submit'
                             variant='text' // 'danger'
+                            size='lg'
                         >
                             Sign up
                         </Button>
