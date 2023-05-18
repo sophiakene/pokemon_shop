83618c464d151c71e9b250bc41c1bf93caa71de8
johannes-hp <johannes@hotmail.com>
2023-05-18 15:37:35 +0200

    Add links to carousel

--- 3-project/poke-app/frontend/src/carousel.tsx
+++ 3-project/poke-app/frontend/src/carousel.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 import './carousel.css';
 
 export function CarouselBanner() {
     return (
         <Carousel variant='dark'>
             <Carousel.Item>
+                <Link to='/detailed_product/Bulbasaur'>
                 <img className='carousel-img'
                 src='/data/poke_images/bulbasaur.avif'
                 alt='Bulbasaur on offer!'
@@ -13,8 +15,10 @@ export function CarouselBanner() {
                 <Carousel.Caption className='justify-content-end'>
                     <h2>Special offer: Bulbasaur on sale!</h2>
                 </Carousel.Caption>
+                </Link>
             </Carousel.Item>
             <Carousel.Item>
+                <Link to='/detailed_product/Diglett'>
                 <img className='carousel-img'
                 src='/data/poke_images/diglett.avif'
                 alt='Diglett on offer!'
@@ -22,6 +26,7 @@ export function CarouselBanner() {
                 <Carousel.Caption className='justify-content-end'>
                     <h2>Special offer: Diglett on sale!</h2>
                 </Carousel.Caption>
+                </Link>
             </Carousel.Item>
         </Carousel>
     )
